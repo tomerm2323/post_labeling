a68368ff4a7b47292ecaf0a6e02c7d987544a901
Tomer Meshulam <tomerm3399@gmail.com>
2023-10-28 21:10:39 +0300

    first commit

--- src/streamlit_form.py
+++ src/streamlit_form.py
@@ -83,9 +83,13 @@ def main():
 
     st.markdown('''How to label? first let's get to know our labels:
     
+    
     1. :violet[Non relevant] - Data that is not the war related at all.
+    
     2. :green[Relevant and pro Israel] - Data that shows Israel in a good light regarding the war.
+    
     3. :red[Relevant and pro Hamas] - Data that shows Israel in a bad light or Hamas in a good light regarding the war.
+    
     4. :gray[Relevant and neutral] - Data that related to the war but takes not obvious side.
     
     Please label each data item(image, text etc...) as it corresponding index in the explanation above.
