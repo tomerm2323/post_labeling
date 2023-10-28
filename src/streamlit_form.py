2f204b0162b7a762ea66bb5bcdf595b1f71ab5b8
Tomer Meshulam <tomerm3399@gmail.com>
2023-10-28 20:50:50 +0300

    first commit

--- src/streamlit_form.py
+++ src/streamlit_form.py
@@ -36,8 +36,28 @@ def get_images():
               obj['Key'].lower().endswith(('.jpg', '.jpeg', '.png', '.gif'))]
 
     return images_urls
+def get_posts_files():
+    bucket_name = "streamlit-posts-labeling"
+    folder_name = 'images'
+    s3 = get_client()
+    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=folder_name)
+    csv_files = [obj['Key'] for obj in response.get('Contents', []) if
+              obj['Key'].lower().endswith('csv')]
+    dataframes = []
+    for csv_file in csv_files:
+        obj = s3.get_object(Bucket=bucket_name, Key=csv_file)
+        dataframe = pd.read_csv(obj['Body'])
+        dataframes.append(dataframe)
 
+    return dataframes
+
+def load_csv_to_dataframe(bucket_name, csv_key):
+    s3 = boto3.client('s3')
+    obj = s3.get_object(Bucket=bucket_name, Key=csv_key)
+    dataframe = pd.read_csv(obj['Body'])
+    return dataframe
 def labeling_component(data, current_row):
+
     st.write(f"**Text:** {data.iloc[current_row, 0]}")
     label = st.text_input(f"Enter label for post {current_row + 1}:", key=f"label_{current_row}")
 
@@ -59,12 +79,6 @@ def video_label_component(path,current_video):
 # Main Streamlit app
 def main():
 
-    # Create connection object and retrieve file contents.
-    # Specify input format is a csv and to cache the result for 600 seconds.
-    conn = st.connection('s3', type=FilesConnection)
-    text_data = conn.read("streamlit-posts-labeling/text/PostsExample.csv", input_format="csv", ttl=600)
-    image_data = conn.open("streamlit-posts-labeling/images/pro israel image.jpg")
-    # st.image(image_data)
     st.title(f"Labeling Form")
 
     st.markdown("""How to label? first let's get to know our labels:
@@ -76,21 +90,18 @@ def main():
                 Please label each data item(image, text etc...) as it corresponding index in the explanation above.\n
                 
                 EXAMPLE: \n post - Israel killing children's will be labeled as 3 because it's against Israel and regarding the war.""")
-    #
-    # csv_file = r"src/PostsExample.csv"
-    # data = pd.read_csv(csv_file, encoding="utf8")
-    # image_data_path = r"C:\Users\Administrator\Desktop\Tomer\personal images"
-    # video_data_path = r"C:\Users\Administrator\Desktop\video"
-    num_entries = len(text_data)
-    labels = []
-    for current_row in range(num_entries):
-        label = labeling_component(text_data, current_row)
-        labels.append(label)
+    label_dict = {"post" : {},"image": {}, "video": {}}
+    posts_files = get_posts_files()
+    for posts_file in posts_files:
+        for current_row in range(len(posts_file)):
+            label = labeling_component(posts_file, current_row)
+            label_dict["post"][posts_file.iloc[current_row, 0]] = label
+
     images_urls = get_images()
-    num_images = len(images_urls)
     for current_image, image_url in enumerate(images_urls):
         label = image_label_component(image_url,current_image,"streamlit-posts-labeling")
-        labels.append(label)
+        label_dict['image'][image_url] = label
+
     # for current_video, path in enumerate(os.listdir(video_data_path)):
     #     label = video_label_component(video_data_path + "/" + path, current_video)
     #     data.loc[len(data.index)] = f"video_{current_video}"
@@ -98,29 +109,29 @@ def main():
     #
     #
     # Check if the number of labels provided matches the number of rows
-    if len(labels) != num_entries + num_images:
-        st.error("Please provide labels for all before submitting.")
+    # if len() != num_entries + num_images:
+    #     st.error("Please provide labels for all before submitting.")
     # if len(labels) != num_entries + len(os.listdir(image_data_path)) + len(os.listdir(video_data_path))  :
     #     st.error("Please provide labels for all rows before submitting.")
-    else:
-        # Save the labeled data to a new CSV file
-        labeled_data = text_data.copy()
-        labeled_data['Label'] = labels
-        st.title("Please review your labels and download")
-        @st.experimental_memo
-        def convert_df(df):
-            return df.to_csv(index=False).encode('utf-8')
+    # else:
+    # Save the labeled data to a new CSV file
 
-        csv = convert_df(labeled_data)
-        st.download_button(
-            "Press to Download",
-            csv,
-            "labeled.csv",
-            "text/csv",
-            key='download-csv'
-        )
-        st.write("Labeled Data:")
-        st.write(labeled_data)
+    labeled_data = pd.DataFrame.from_dict(label_dict)
+    st.title("Please review your labels and download")
+    @st.experimental_memo
+    def convert_df(df):
+        return df.to_csv(index=False).encode('utf-8')
+
+    csv = convert_df(labeled_data)
+    st.download_button(
+        "Press to Download",
+        csv,
+        "labeled.csv",
+        "text/csv",
+        key='download-csv'
+    )
+    st.write("Labeled Data:")
+    st.write(labeled_data)
 
 if __name__ == "__main__":
     main()
