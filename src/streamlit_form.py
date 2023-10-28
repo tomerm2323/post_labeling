cda43a7fba8807b0b5e61d3b8cecfbbe4b285ac8
Tomer Meshulam <tomerm3399@gmail.com>
2023-10-28 20:08:41 +0300

    first commit

--- src/streamlit_form.py
+++ src/streamlit_form.py
@@ -39,7 +39,7 @@ def get_images():
 
 def labeling_component(data, current_row):
     st.write(f"**Text:** {data.iloc[current_row, 0]}")
-    label = st.text_input(f"Enter label (0 or 1) for row {current_row + 1}:", key=f"label_{current_row}")
+    label = st.text_input(f"Enter label for post {current_row + 1}:", key=f"label_{current_row}")
 
     return label
 
@@ -48,13 +48,13 @@ def image_label_component(image_url,current_image,bucket_name):
     img_data = s3.get_object(Bucket=bucket_name, Key=image_url)
     img = Image.open(BytesIO(img_data['Body'].read()))
     st.image(img, caption="Image from S3", use_column_width=True)
-    label = st.text_input(f"image {current_image}:", key=f"label_image_{current_image}")
+    label = st.text_input(f"Enter label for image {current_image}:", key=f"label_image_{current_image}")
     return label
 def video_label_component(path,current_video):
     video_file = open(path, 'rb')
     video_bytes = video_file.read()
     st.video(video_bytes)
-    label = st.text_input(f"Enter label (0 or 1) for video {current_video}:", key=f"label_video_{current_video}")
+    label = st.text_input(f"Enter label for video {current_video}:", key=f"label_video_{current_video}")
     return label
 # Main Streamlit app
 def main():
@@ -65,7 +65,16 @@ def main():
     text_data = conn.read("streamlit-posts-labeling/text/PostsExample.csv", input_format="csv", ttl=600)
     image_data = conn.open("streamlit-posts-labeling/images/pro israel image.jpg")
     # st.image(image_data)
-    st.title(f"Text Labeling Form\n Please enter 1 for pro israel and 0 otherwise.....")
+    st.title(f"Labeling Form\n")
+    st.markdown("How to label?\n first let's get to know our labels:"
+                "1. violet[Non relevant] - Data that is not the war related at all\n"
+                "2. green[Relevant and pro Israel] - Data that shows Israel in a good light regarding the war\n"
+                "3. red[Relevant and pro Hamas] - Data that shows Israel in a bad light or Hamas in a good light regarding the war\n"
+                "4. gray[Relevant and neutral] - Data that related to the war but takes not obvious side\n"
+                ""
+                "Please label each data item(image, text etc...) as it corresponding index in the explanation above.\n"
+                ""
+                "EXAMPLE: \n post - Israel killing children's will be labeled as 3 because it's against Israel and regarding the war.")
     #
     # csv_file = r"src/PostsExample.csv"
     # data = pd.read_csv(csv_file, encoding="utf8")
@@ -77,6 +86,7 @@ def main():
         label = labeling_component(text_data, current_row)
         labels.append(label)
     images_urls = get_images()
+    num_images = len(images_urls)
     for current_image, image_url in enumerate(images_urls):
         label = image_label_component(image_url,current_image,"streamlit-posts-labeling")
         labels.append(label)
@@ -87,8 +97,8 @@ def main():
     #
     #
     # Check if the number of labels provided matches the number of rows
-    if len(labels) + len(images_urls) != num_entries :
-        st.error("Please provide labels for all rows before submitting.")
+    if len(labels) != num_entries + num_images:
+        st.error("Please provide labels for all before submitting.")
     # if len(labels) != num_entries + len(os.listdir(image_data_path)) + len(os.listdir(video_data_path))  :
     #     st.error("Please provide labels for all rows before submitting.")
     else:
